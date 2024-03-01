cceacef9886dc952dcef94dd381fd72188a815c4
SantiagoDeniz <santiagodenizinf@gmail.com>
2024-03-01 17:32:03 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+/*alert("Hola este es mi Javascript");
+var nombre = "Santiago";
+console.log(nombre);*/
+
+/*let contenidoTitulo = "Nombre";
+
+let titulo = document.querySelector(".logo .fuente-")
+titulo.innerHTML = contenidoTitulo;
+
+//CONDICIONALES
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo);
+
+if(textoTitulo == "Nombre") {
+    titulo.innerHTML = "Otro";
+} else {
+    console.log("no se cumple");
+} */
+
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const parrafo = document.getElementById("alertas");
+
+function validarFormulario() {
+  let warnings = "";
+  let valido = true;
+  parrafo.innerHTML = "";
+
+  if (nombre.value.length < 4) {
+    warnings += `El nombre debe contener más de 4 carcateres`;
+    valido = false;
+  }
+
+  if (!valido) {
+    parrafo.innerHTML = warnings;
+  } else {
+    parrafo.innerHTML = "Enviado";
+  }
+  return valido;
+}
+
+form.addEventListener("submit", (e) => {
+  if (validarFormulario()) {
+    // Si la validación es exitosa, puedes enviar el formulario
+    formulario.submit();
+  } else {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+  }
+});
